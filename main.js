5859ab44b74cc030fd4d65c4ad934b5a18ff9c11
Ekaterina S. Zubekhina <zubekhina-es@mail.ru>
2020-12-02 15:39:55 +0300

    chore: Regular changes and additions for the site

--- main.js
+++ main.js
@@ -40,6 +40,31 @@ function traverseChildren(elem) {
 
 //toggle();
 //using closure to cache all child elements
-var parent = document.getElementById("navigation");
+var parent = document.getElementById('navigation');
 parent.addEventListener('mouseout', makeMouseOutFn(parent), true);
 
+//печатающийся текст
+var textL = 'На данный момент я обучаюсь профессии frontend-разработчик на образовательном IT-портале - ' +
+    'GeekBrains | GeekUniversity. Учусь верстать сайты и лендинги,' +
+    ' писать скрипты на языке программирования JavaScript. Хочу найти работу или стажировку, чтобы ' +
+    ' дальше развиваться и узнавать что-то новое в этой области.' +
+    ' Для меня создание сайтов - это волшебство! Я каждый день учусь и продолжаю стремиться стать в' +
+    ' будущем отличным специалистом в мире разработки!';
+
+function printed_el_text(el) {
+    // var text = el.innerHTML,
+    i = 0,
+        printF = function () {
+            i++;
+            if (i <= textL.length) {
+                el.innerHTML = textL.substr(0, i);
+                setTimeout(printF, 60);
+            }
+        };
+    printF();
+};
+//for (; ;) {
+//setTimeout(function () {
+printed_el_text(document.getElementById("about-me-text-blok"));
+    //}, 8000);
+//}
